a9bf927d888c1191746e68a4de2b8369c125fc47
wusu <1044205037@qq.com>
2016-11-30 18:56:43 +0800

    创建版本

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
 # RuntimePermissionsDemo
-基于PermissionsDispatcher的动态权限demo，PermissionsDispatcher地址https://github.com/hotchemi/PermissionsDispatcher
+基于PermissionsDispatcher的动态权限demo，PermissionsDispatcher地址https://github.com/hotchemi/PermissionsDispatcher   
+（部分定制rom体验有些许不同，例如华为emui）
+# 快速配置 
+## 1.引入jia包 
+
+```
+apply plugin: 'android-apt'
+
+dependencies {
+  compile 'com.github.hotchemi:permissionsdispatcher:2.2.0'
+  apt 'com.github.hotchemi:permissionsdispatcher-processor:2.2.0'
+}
+```  
+## 2.配置   
+在工程project目录 **build.gradle** 文件下添加 ： 
+
+```
+buildscript {
+  dependencies {
+    classpath 'com.neenbedankt.gradle.plugins:android-apt:1.8'
+  }
+}
+```  
+# 开始使用  
+## 1.添加注解  
+注解列表（总共也只有这几个注解）：  
+> Tip：注解的方法不能为 private
+
+
+注解 | 必须 | 说明
+---|---|---
+@RuntimePermissions | √ | 注册，表示在Activity或Fragment中处理动态权限
+@NeedsPermission | √ | 注释一个执行时需要一个或多个权限的方法
+@OnShowRationale | | 注释一个说明方法，该方法说明为什么需要权限，该方法通过一个 **PermissionRequest** 对象可以终止或继续此权限请求
+@OnPermissionDenied | | 当用户拒绝权限时调用此注解注释的方法
+@OnNeverAskAgain | | 当用户勾选了“不再询问”后又调用检测权限时调用此注解注释的方法
+
+```
+@RuntimePermissions
+public class MainActivity extends AppCompatActivity {
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    void showCamera() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.sample_content_fragment, CameraPreviewFragment.newInstance())
+                .addToBackStack("camera")
+                .commitAllowingStateLoss();
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForCamera(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+            .setMessage(R.string.permission_camera_rationale)
+            .setPositiveButton(R.string.button_allow, (dialog, button) -> request.proceed())
+            .setNegativeButton(R.string.button_deny, (dialog, button) -> request.cancel())
+            .show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void showDeniedForCamera() {
+        Toast.makeText(this, R.string.permission_camera_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void showNeverAskForCamera() {
+        Toast.makeText(this, R.string.permission_camera_neverask, Toast.LENGTH_SHORT).show();
+    }
+}
+```
+## 2.生成帮助类  
+在编译时， PermissionsDispatcher会产生一个类 **Mainactivitypermissionsdispatcher** ([Activity Name] + permissionsdispatcher)，你可以用它来安全地访问这些权限保护方法。酱紫：
+
+```
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    findViewById(R.id.button_camera).setOnClickListener(v -> {
+      // NOTE: delegate the permission handling to generated method
+      MainActivityPermissionsDispatcher.showCameraWithCheck(this);
+    });
+    findViewById(R.id.button_contacts).setOnClickListener(v -> {
+      // NOTE: delegate the permission handling to generated method
+      MainActivityPermissionsDispatcher.showContactsWithCheck(this);
+    });
+}
+
+@Override
+public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    // NOTE: delegate the permission handling to generated method
+    MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+}
+```
+## 如果你用 AndroidAnnotations  
+你需要：  
+在项目project的 **build.gradle** 中添加  
+
+```
+allprojects {
+    repositories {
+        ...
+        maven { url "https://jitpack.io" }
+    }
+}
+```
+在项目moudle的 **build.gradle** 中添加 
+
+```
+dependencies {
+    compile 'com.github.hotchemi:permissionsdispatcher:2.1.3'
+    apt 'com.github.hotchemi:permissionsdispatcher-processor:2.1.3'
+    compile 'org.androidannotations:androidannotations-api:4.0.0'
+    apt 'org.androidannotations:androidannotations:4.0.0'
+    apt 'com.github.AleksanderMielczarek:AndroidAnnotationsPermissionsDispatcherPlugin:0.1.0'
+}
+```
+更多详情：https://github.com/hotchemi/PermissionsDispatcher   
---
+++ src/main/java/com/wuxiaosu/runtimepermissionsdemo/MainActivity.java
@@ -0,0 +1,197 @@
+package com.wuxiaosu.runtimepermissionsdemo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final static private String TAG = "RuntimePermissionsDemo";
+    private Button mBtnCallPhone;
+    private Button mBtnLocation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mBtnCallPhone = (Button) findViewById(R.id.btn_call_phone);
+        mBtnLocation = (Button) findViewById(R.id.btn_location);
+
+        mBtnCallPhone.setOnClickListener(this);
+        mBtnLocation.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(@NonNull View v) {
+        switch (v.getId()) {
+            case R.id.btn_call_phone:
+                MainActivityPermissionsDispatcher.showCallPhoneWithCheck(this);
+                break;
+            case R.id.btn_location:
+                MainActivityPermissionsDispatcher.showLocationWithCheck(this);
+                break;
+        }
+    }
+
+
+    @NeedsPermission(Manifest.permission.CALL_PHONE)
+    void showCallPhone() {
+        Log.e(TAG, "========================== >> 打电话");
+        //打电话
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "10010"));
+        startActivity(intent);
+    }
+
+    @NeedsPermission({Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE})
+    void showLocation() {
+        Log.e(TAG, "========================== >> 定位");
+        //定位
+    }
+
+    /**
+     * 向用户展示申请权限的理由
+     *
+     * @param request
+     */
+    @OnShowRationale({Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE})
+    void showRationaleForLocation(PermissionRequest request) {
+        Log.e(TAG, "showRationaleForLocation: ===================== >> 展示申请理由");
+        showRationaleDialog(R.string.permission_location_rationale, request);
+    }
+
+    /**
+     * 权限被拒绝
+     */
+    @OnPermissionDenied({Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE})
+    void onLocationDenied() {
+        Log.e(TAG, "onLocationDenied: ===================== >> 权限被拒绝");
+        Toast.makeText(this, R.string.permission_location_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 向用户展示申请权限的理由
+     *
+     * @param request
+     */
+    @OnShowRationale(Manifest.permission.CALL_PHONE)
+    void showRationaleForCallPhone(PermissionRequest request) {
+        Log.e(TAG, "showRationaleForCallPhone: ===================== >> 展示申请理由");
+        showRationaleDialog(R.string.permission_call_phone_rationale, request);
+    }
+
+    /**
+     * 权限被拒绝
+     */
+    @OnPermissionDenied(Manifest.permission.CALL_PHONE)
+    void onCallPhoneDenied() {
+        Log.e(TAG, "onCallPhoneDenied: ===================== >> 权限被拒绝");
+        Toast.makeText(this, R.string.permission_call_phone_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 用户勾选了不再询问
+     */
+    @OnNeverAskAgain(Manifest.permission.CALL_PHONE)
+    void onCallPhoneNeverAskAgain() {
+        Log.e(TAG, "onCallPhoneNeverAskAgain: ===================== >> 被设置不再询问");
+        Toast.makeText(this, R.string.permission_call_phone_never_askagain, Toast.LENGTH_SHORT).show();
+        //给一个弱提示 或者弹出对话框让用户去设置界面
+       /* new AlertDialog.Builder(this)
+                .setPositiveButton("去啊,去啊", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        sendSettingsIntent(DemoActivity.this);
+                    }
+                })
+                .setNegativeButton("不去,滚", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                    }
+                })
+                .setCancelable(false)
+                .setMessage("是不是现在去设置权限?")
+                .show();*/
+    }
+
+    private void showRationaleDialog(@StringRes int messageResId, final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage(messageResId)
+                .show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    /**
+     * 显示应用信息界面 (去设置权限)
+     *
+     * @param context
+     * @return 有可能打开失败
+     */
+    public static boolean sendSettingsIntent(Context context) {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", context.getPackageName(), null));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        try {
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
